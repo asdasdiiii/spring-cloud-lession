ee25cc0e579fb80c0c27dddcfe2a123c1e0c7448
hegh <hegh@kamfu.com.cn>
2019-06-26 18:22:32 +0800

    学习代码1

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user(id,username,name,age,balance) values (1,'user1','张三',20,100.00);
+insert into user(id,username,name,age,balance) values (2,'user2','李四',20,100.00);
+insert into user(id,username,name,age,balance) values (3,'user3','何三',20,100.00);
+insert into user(id,username,name,age,balance) values (4,'user4','杜四',20,100.00);
