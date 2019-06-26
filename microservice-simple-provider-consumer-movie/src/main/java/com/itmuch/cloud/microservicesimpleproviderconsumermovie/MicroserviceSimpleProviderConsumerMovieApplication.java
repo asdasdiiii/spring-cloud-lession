b53cb5e53d3d06a29d2900d5a0f23165809755ba
hegh <hegh@kamfu.com.cn>
2019-06-26 18:32:58 +0800

    学习代码1

---
+++ microservice-simple-provider-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleproviderconsumermovie/MicroserviceSimpleProviderConsumerMovieApplication.java
@@ -0,0 +1,13 @@
+package com.itmuch.cloud.microservicesimpleproviderconsumermovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSimpleProviderConsumerMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSimpleProviderConsumerMovieApplication.class, args);
+	}
+
+}
